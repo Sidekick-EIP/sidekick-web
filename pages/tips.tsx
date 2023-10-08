3db7657db5e781cecf975f3945c8daabdb9f22f1
guts2 <pierre.bittou@epitech.eu>
2023-10-08 22:09:40 +0200

    [+] : creating the tips page

---
+++ pages/tips.tsx
@@ -0,0 +1,12 @@
+"use client";
+import React from 'react';
+import Faqcomponent from "../components/Faqcomponent";
+
+export default function Tips() {
+
+    return (
+        <div >
+            herl
+        </div>
+    )
+  }
